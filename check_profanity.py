0e1bebf6f997f83e6490ddbfc32f2ea32f9e3945
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-25 13:08:56 +0200

    start

--- check_profanity.py
+++ check_profanity.py
@@ -1,1 +1,8 @@
-print("hello")
+def read_text():
+    quotes = open("/Users/Daniela/Desktop/Important shit?/test_text.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+
+
+read_text()
