a5d446b37b03d2380943bb744ec68c3727c0c144
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-28 10:22:09 +0200

    like i have seriously given up

--- check_profanity.py
+++ check_profanity.py
@@ -1,7 +1,4 @@
-def check_profanity(text_to_check):
-    from urllib.request import urlopen
-    html = urlopen('http://www.wdylike.appspot.com/?q=QUERY' + text_to_check).read()
-    print(html)
+from urllib.request import urlopen
 
 
 def read_text():
@@ -10,6 +7,19 @@ def read_text():
     print(contents_of_file)
     quotes.close()
     check_profanity(contents_of_file)
+
+
+def check_profanity(text_to_check):
+    connection = urlopen("http://www.wdylike.appspot.com/?q=shot" + text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if True in output:
+        print("Profanity Alert!!")
+    elif False in output:
+        print("This document has no curse words!")
+    else:
+        print("Could not scan tha document properly.")
 
 
 read_text()
