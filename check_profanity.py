a36a4858f040efae312754222e23d0dc785bc4f9
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-28 10:13:41 +0200

    like seriously

--- check_profanity.py
+++ check_profanity.py
@@ -1,13 +1,11 @@
-import webbrowser
-
-
 def check_profanity(text_to_check):
-    html = webbrowser.open('http://www.wdylike.appspot.com/?q=QUERY' + text_to_check).read()
+    from urllib.request import urlopen
+    html = urlopen('http://www.wdylike.appspot.com/?q=QUERY' + text_to_check).read()
     print(html)
 
 
 def read_text():
-    quotes = open("/Users/Daniela/Desktop/Important shit?/test_text.txt")
+    quotes = open("/Users/Daniela/Desktop/Coding stuff/test_text.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
